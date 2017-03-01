c53bacea47b441734a5b6f29529dc610d1ded515
Seb Dancer-Michel <s.dancermichel@gmail.com>
2017-03-01 23:14:14 +0100

    [add] basic flow
    and many other things :
    - fonts
    - variables
    - onload JS check

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,19 @@
 (function () {
+  // Checking if browser supports Javascript
   var doc = document.documentElement;
-
   doc.classList.remove('no-js');
   doc.classList.add('js');
 
-  console.log('hello');
+  var bubblesCollection = document.querySelectorAll('.conversation__bubble--general');
+  var i = 0;
+  // bubblesCollection[0].classList.remove('hidden');
+
+  function nextBubble () {
+    changeBubble(bubblesCollection[i]);
+    i = (i + 1) % bubblesCollection.length;
+  };
+  function changeBubble () {
+    bubblesCollection[i].classList.remove('hidden');
+  };
+  window.setTimeout(window.setInterval(nextBubble, 3000), 1000);
 }());
