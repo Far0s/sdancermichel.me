226ef782258e1c6d56f4cefdb6ffb2602fef9c28
Seb Dancer-Michel <s.dancermichel@gmail.com>
2017-03-01 23:50:31 +0100

    [add] reactions component
    + fix package.json

--- src/js/main.js
+++ src/js/main.js
@@ -5,15 +5,16 @@
   doc.classList.add('js');
 
   var bubblesCollection = document.querySelectorAll('.conversation__bubble--general');
-  var i = 0;
-  // bubblesCollection[0].classList.remove('hidden');
-
-  function nextBubble () {
-    changeBubble(bubblesCollection[i]);
-    i = (i + 1) % bubblesCollection.length;
-  };
-  function changeBubble () {
+  // var i = 0;
+  for (var i = 0; i < bubblesCollection.length; i++) {
     bubblesCollection[i].classList.remove('hidden');
   };
-  window.setTimeout(window.setInterval(nextBubble, 3000), 1000);
+  // function nextBubble () {
+  //   changeBubble(bubblesCollection[i]);
+  //   i = (i + 1) % bubblesCollection.length;
+  // };
+  // function changeBubble () {
+  //   bubblesCollection[i].classList.remove('hidden');
+  // };
+  // window.setTimeout(window.setInterval(nextBubble, 3000), 1000);
 }());
