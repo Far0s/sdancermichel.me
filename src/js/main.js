2087e520a6f02ed78fbda1d8831a83755201ca76
Seb Dancer-Michel <s.dancermichel@gmail.com>
2017-03-03 02:55:45 +0100

    [add] loading bubble and behavior

--- src/js/main.js
+++ src/js/main.js
@@ -5,13 +5,14 @@
   doc.classList.add('js');
 
   var bubblesCollection = document.querySelectorAll('.conversation__bubble--general, .conversation .reaction__button');
-  // var reactionsCollection = document.querySelector('.reactions');
+  var reactionsCollection = document.querySelector('.reactions');
+  var loaderBubble = document.querySelector('.conversation__bubble--loader');
 
   // For debugging
   // for (var i = 0; i < bubblesCollection.length; i++) {
   //   bubblesCollection[i].classList.remove('hidden');
   // };
-  // reactionsCollection.classList.remove('hidden');
+  reactionsCollection.classList.remove('hidden');
 
   var i = 0;
   function nextBubble () {
@@ -20,6 +21,12 @@
   };
   function changeBubble () {
     bubblesCollection[i].classList.remove('hidden');
+    console.log(i);
+    if (i === 4) {
+      console.log('oh');
+      clearInterval(displayBubbles);
+      loaderBubble.classList.add('hidden');
+    }
   };
   var displayBubbles = window.setInterval(nextBubble, 2000);
   window.setTimeout(displayBubbles, 1000);
